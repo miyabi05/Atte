3ff0825050e83e39d526e6b20c8e15787fb9ad29
miyabi05 <miyabi.m.0511@gmail.com>
2024-03-04 02:43:09 +0900

    ログイン会員登録打刻ビュー画面作成

--- src/database/migrations/2024_03_03_014323_create_register_table.php
+++ src/database/migrations/2024_03_03_014323_create_register_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRegisterTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('register', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('email');
             $table->string('password');
+            $table->string('password_confirmation');
             $table->timestamp('created_at')->useCurrent()->nullable();
             $table->timestamp('updated_at')->useCurrent()->nullable();
         });
@@ -29,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('register');
     }
 }
